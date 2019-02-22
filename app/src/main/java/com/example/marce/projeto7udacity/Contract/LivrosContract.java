e577e4c85fdb1bc0a86c57811000b836fba23e15
Marcelo Souza <marcelo.souza64@fatec.sp.gov.br>
2019-02-21 21:56:42 -0300

    Aplicação Teste Funcional

--- app/src/main/java/com/example/marce/projeto7udacity/Contract/LivrosContract.java
+++ app/src/main/java/com/example/marce/projeto7udacity/Contract/LivrosContract.java
@@ -11,19 +11,21 @@ public class LivrosContract implements BaseColumns {
     public static final String COLUNA_MODELO = "modelo";
     public static final String COLUNA_PREÇO = "preço";
     public static final String COLUNA_QUANTIDADE = "quantidade";
+    public static final String COLUNA_FORNECEDOR="fornecedor";
+    public static final String COLUNA_TELEFONE="telefone";
 
 
     public static final int MODELO_DESCONHECIDO = 0;
     public static final int MODELO_LITERATURA = 1;
-    public static final int MODELO_INFANTIL = 2;
-
 
     public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " +TABLE_NAME +
             " (" + COLUNA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT , " +
             COLUNA_NOME_LIVRO + " TEXT_TYPE ," +
-            COLUNA_MODELO + " TEXT_TYPE ," +
+            COLUNA_MODELO + " INTEGER ," +
             COLUNA_PREÇO + " REAL ," +
-            COLUNA_QUANTIDADE + " INTEGER);";
+            COLUNA_QUANTIDADE + " INTEGER ,"+
+            COLUNA_FORNECEDOR+" TEXT_TYPE ,"+
+            COLUNA_TELEFONE+" TEXT_TYPE);";
 
     public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "+TABLE_NAME;
 
